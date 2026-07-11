ccdb99690f0719395a04fc396c7030886397216f
unknown <your-github-email@example.com>
2026-07-11 22:38:43 +0600

    Added Course Management API Lab 01

---
+++ course-management-api/src/course/course.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class CourseService {
+
+    getAllCourses() {
+        return "Get All Courses - from Service ";
+    }
+
+    getCourseByID(id: string) {
+        return `Get Course with ID: ${id} from Service`;
+    }
+
+    createCourse() {
+        return "Create Course - from Service";
+    }
+
+    updateCourse(id: string) {
+        return `Update Course ${id} - from Service`;
+    }
+
+    patchCourse(id: string) {
+        return `Patch Course ${id} - from Service`;
+    }
+
+    deleteCourse(id: string) {
+        return `Delete Course ${id} - from Service`;
+    }
+
+}
